a1a4687469ec459478c76a277c94c256babfa9e5
scarlett07 <scarlett.geo@gmail.com>
2018-03-06 13:34:02 -0600

    agregando links de personajes por película

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 let containerFilms = $('#container-films');
-
-function getData() {
+const pelis ="https://swapi.co/api/films/";
+function getData(url) {
   $.ajax({
     dataType: "json",
-    url: "https://swapi.co/api/films/"
+    url: url
   }).done(drawData)
     .fail(function(status) {
     console.log("error");
@@ -14,9 +14,14 @@ function drawData(data) {
   const films = data.results;
   console.log(films);
   let cardMovies = ' ';
+  let characters = ' ';
   films.forEach(function(element) {
     const movie = element.title;
     const resumen = element.opening_crawl;
+    const personajes = element.characters;
+    personajes.forEach(personaje=>{
+      characters += `<p><a href="#">${personaje}</a></p>`
+    })
   //  const poster = 'https://dummyimage.com/150x150';
     cardMovies += `<div class="card">
       <div class="card-image waves-effect waves-block waves-light">
@@ -27,14 +32,12 @@ function drawData(data) {
         <p>${resumen}</p>
       </div>
       <div class="card-reveal">
-        <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-        <p><a href="#">This is a link</a></p>
-        <p><a href="#">This is a link</a></p>
-      </div>
-`;
+        <span class="card-title grey-text text-darken-4">${movie} <span>Characters</span><i class="material-icons right">close</i></span>
+        ${characters}
+        </div>`;
+      console.log(personajes);
   })
-  console.log(cardMovies);
   containerFilms.html(cardMovies)
 };
 
-getData();
+getData(pelis);
