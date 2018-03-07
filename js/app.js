3c823d99f974316b1451d472d784c198e06c43bd
scarlett07 <scarlett.geo@gmail.com>
2018-03-07 08:42:03 -0600

    Un poco de estilo y ajustes en funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 let containerFilms = $('#container-films');
-const pelis ="https://swapi.co/api/films/";
+const pelis = "https://swapi.co/api/films/";
+
 function getData(url) {
   $.ajax({
-    dataType: "json",
-    url: url
-  }).done(drawData)
+      dataType: "json",
+      url: url
+    }).done(drawData)
     .fail(function(status) {
-    console.log("error");
-  })
+      console.log("error");
+    })
 };
 
 function drawData(data) {
@@ -15,29 +16,31 @@ function drawData(data) {
   console.log(films);
   let cardMovies = ' ';
   let characters = ' ';
+  const poster = './assets/images/star-wars.jpg';
   films.forEach(function(element) {
     const movie = element.title;
     const resumen = element.opening_crawl;
     const personajes = element.characters;
-    personajes.forEach(personaje=>{
+    personajes.forEach(personaje => {
       characters += `<p><a href="#">${personaje}</a></p>`
     })
-  //  const poster = 'https://dummyimage.com/150x150';
-    cardMovies += `<div class="card">
-      <div class="card-image waves-effect waves-block waves-light">
-        <img class="activator" src="images/office.jpg">
-      </div>
-      <div class="card-content">
-        <span class="card-title activator grey-text text-darken-4">${movie}<i class="material-icons right">more_vert</i></span>
-        <p>${resumen}</p>
-      </div>
-      <div class="card-reveal">
-        <span class="card-title grey-text text-darken-4">${movie} <span>Characters</span><i class="material-icons right">close</i></span>
-        ${characters}
-        </div>`;
-      console.log(personajes);
+    cardMovies += `<div class ="row">
+    <div class="card col s12 m8 l6 ">
+    <div class="card-image waves-effect waves-block waves-light">
+    <img class="activator" src=${poster}>
+    </div>
+    <div class="card-content">
+    <span class="card-title activator grey-text text-darken-4">${movie}<i class="material-icons right">more_vert</i></span>
+    <p>${resumen}</p>
+    </div>
+    <div class="card-reveal">
+    <span class="card-title grey-text text-darken-4">${movie} <span>Characters</span><i class="material-icons right">close</i></span>
+    ${characters}
+    </div>
+    </div>`;
+    containerFilms.html(cardMovies)
   })
-  containerFilms.html(cardMovies)
 };
+//console.log(cardMovies);
 
 getData(pelis);
